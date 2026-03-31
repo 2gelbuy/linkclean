2aa51d30f8d77b611bee593625c66917b39ed343
Tugelbay Konabayev <t.konabayev@gmail.com>
2026-03-31 21:15:16 +0000

    agent: auto-commit before sync

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "wxt";
 
 const EXT_NAME = "__MSG_extName__";
 const EXT_DESC = "__MSG_extDesc__";
-const EXT_VERSION = "1.2.0";
+const EXT_VERSION = "1.3.0";
 const EXT_HOMEPAGE = "https://konabayev.com/linkclean/";
 
 export default defineConfig({
